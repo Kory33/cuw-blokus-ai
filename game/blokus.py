3018194c3bf9c0d89d388a9c65b4278fcfb223d7
Kory33 <konryoitdv@gmail.com>
2017-02-25 00:33:45 +0100

    delete placement structure

--- game/blokus.py
+++ game/blokus.py
@@ -30,10 +30,10 @@ class BlokusSquareData(Enum):
             self is BlokusSquareData.BLUE_5
 
     @staticmethod
-    def get_data(placement):
+    def get_data(placement, is_red):
         """Obtain the data corresponding to the placement"""
-        target_num = len(placement.get_placement())
-        if not placement.is_red:
+        target_num = len(placement)
+        if not is_red:
             target_num *= -1
 
         return BlokusSquareData(target_num)
@@ -147,49 +147,22 @@ class BlokusBoard:
                 self._is_first_cell_covered(placement) and
                 self._is_placement_compatible(placement))
 
-    def place(self, blokus_placement):
+    def place(self, blokus_placement_list):
         """
         Execute a given placement.
         Returns True when executed successfully.
 
         Raises InvalidPlacementError when the placement is invalid.
         """
-        placements_list = blokus_placement.get_placement_list()
+        if not self._is_placement_valid(blokus_placement_list):
+            raise InvalidPlacementError(self, blokus_placement_list)
 
-        if not self._is_placement_valid(placements_list):
-            raise InvalidPlacementError(self, blokus_placement)
-
-        placement_data = BlokusSquareData.get_data(blokus_placement)
-        for placement in placements_list:
-            self._board[placement[0]][placement[1]] = placement_data
+        placement_data = BlokusSquareData.get_data(blokus_placement_list, self.is_red_next)
+        for coord in blokus_placement_list:
+            self._board[coord[0]][coord[1]] = placement_data
 
         self.is_red_next = not self.is_red_next
         return True
-
-
-class BlokusPlacement:
-    """
-    Class which represents a placement.
-    A placement is created once in each player turn, hence is a step in the game.
-    """
-
-    def __init__(self, placement_array, board_state):
-        """
-        placement_array is a two-dimentional array which stores pairs of coordinates.
-        board_state is a status of the game board before the placement takes place.
-
-        Raises an InvalidPlacementError when an illegal placement is given to the constructor.
-        """
-        self._placement = sorted(placement_array, lambda coord: coord[0])
-        self._old_board_state = board_state
-
-    def get_placement_list(self):
-        """Get the placement list"""
-        return self._placement[::]
-
-    def is_red(self):
-        """Returns true if the placement will be executed by the red."""
-        return self._old_board_state.is_red_next
 
 class BlokusGame:
     """Class which represents a game session."""
