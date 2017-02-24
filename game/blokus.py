4f05bb7e492a2e59fac0dbfaa5fb05da461fc4c0
Kory33 <konryoitdv@gmail.com>
2017-02-24 10:42:27 +0100

    add blokus placement data class

--- game/blokus.py
+++ game/blokus.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from blokus_exception import InvalidPlacementError
+
 
 class BlokusSquareData(Enum):
     EMPTY = 0
@@ -37,8 +39,60 @@ class BlokusBoard:
         return self._size
 
 
+class BlokusPlacement:
+    """Class which represents a placement.
+    A placement is created once in each player turn, hence is a step in the game.
+
+    placement_array is a two-dimentional array which stores pairs of coordinates.
+    Raises an InvalidPlacementError when an illegal placement is given to the constructor."""
+
+    def __init__(self, placement_array, board_state):
+        self._placement = sorted(placement_array, lambda coord: coord[0])
+        self._old_board_state = board_state
+
+        if not self._is_placement_valid():
+            raise InvalidPlacementError(board_state, placement_array)
+
+    def _is_placement_valid(self):
+        placement_num = len(self._placement)
+        if placement_num < 3 or placement_num > 5:
+            return False
+
+        return self._is_placement_continuous()
+
+    def _is_placement_continuous(self):
+        placement_num = len(self._placement)
+
+        # scan the board towards positive x and see if each y's are continuous
+        # assuming that the placement_array is already sorted by ascending-x values
+        previous_y_values = []
+        scan_x_coord = self._placement[0][0]
+        placement_counter = 0
+
+        while True:
+            y_values = []
+
+            while self._placement[placement_counter][0] == scan_x_coord:
+                placement = self._placement[placement_counter]
+                placement_y = placement[1]
+
+                # if the current column is not connected to the previous column
+                if placement_y not in previous_y_values:
+                    return False
+
+                y_values.append(placement_y)
+                placement_counter += 1
+
+                # when all the placements are checked
+                if placement_counter == placement_num:
+                    return True
+
+            scan_x_coord += 1
+            previous_y_values = y_values
+
+
 class BlokusGame:
     """Class which represents a game session."""
 
-    def __init__():
-        pass
+    def __init__(self, board_size=12):
+        self._board = BlokusBoard(size=board_size)
