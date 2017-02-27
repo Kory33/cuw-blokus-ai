1f4e1a5ce16dc77aa85e8940017d184766819b26
Kory33 <konryoitdv@gmail.com>
2017-02-27 07:33:15 +0100

    fix unnecessary math module refference

--- game/distance.py
+++ game/distance.py
@@ -1,11 +1,8 @@
-import math
-
-
 def manhattan_2d(start_vec, end_vec):
     """Returns a Manhattan distance between two given points."""
-    return math.abs(start_vec[0] - end_vec[0]) + math.abs(start_vec[1] - end_vec[1])
+    return abs(start_vec[0] - end_vec[0]) + abs(start_vec[1] - end_vec[1])
 
 
 def chebyshev_2d(start_vec, end_vec):
     """Returns a Chebyshev distance between two given points."""
-    return max(math.abs(start_vec[0] - end_vec[0]), math.abs(start_vec[1] - end_vec[1]))
+    return max(abs(start_vec[0] - end_vec[0]), abs(start_vec[1] - end_vec[1]))
