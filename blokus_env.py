ccc2c6ab64cfd6180ba1b7ebef5fcac9a691b582
Kory33 <konryoitdv@gmail.com>
2017-03-03 15:04:59 +0100

    implement coloring on console output

--- blokus_env.py
+++ blokus_env.py
@@ -2,6 +2,7 @@
 A module which provides a learning environment for blokus agents.
 """
 from game.blokus import BlokusGame
+from termcolor import colored
 
 class BlokusEnvironment:
     """
@@ -33,7 +34,7 @@ class BlokusEnvironment:
 
             # execute learning cycle
             if self.prev_actions[agent_color] is not None:
-                reward = len(self.prev_actions[opponent_color] - self.prev_actions[agent_color])
+                reward = 0
                 agent.learn(reward, self.session, False)
 
             is_game_continued = True
@@ -76,9 +77,9 @@ class BlokusEnvironment:
             for column in range(size):
                 cell_data = board.get_data_at((column, row))
                 if cell_data.is_red():
-                    print("R", end="")
+                    print(colored("R", "red"), end="")
                 elif cell_data.is_blue():
-                    print("B", end="")
+                    print(colored("B", "blue"), end="")
                 else:
                     print("-", end="")
             print()
